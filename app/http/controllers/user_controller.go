590880e524409dc48b44b60d367184861baed4c0
Bowens <hwbrzzl@qq.com>
2023-03-19 19:37:59 +0800

    Add gRPC

--- app/http/controllers/user_controller.go
+++ app/http/controllers/user_controller.go
@@ -7,8 +7,13 @@ import (
 )
 
 /*********************************
+gRPC Example
+
 This is the gRPC Client side, if you need the full steps about gRPC, please visit the link below.
 https://github.com/goravel/example/blob/master/app/grpc/controllers/user_controller.go
+
+There is a client interceptor example about opentracing, you can find it in `app/grpc/interceptors/opentracing_client.go`
+[gRPC Document](https://www.goravel.dev/the-basics/grpc.html)
  ********************************/
 
 type UserController struct {
